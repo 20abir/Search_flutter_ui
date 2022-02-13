374fddc71ca896bf59da4b6d3ecd0f781d8cba06
20abir <tawhid.lubnan@gmail.com>
2022-02-13 13:23:08 +0600

    search2

--- lib/ExtraFile/search_2_file.dart
+++ lib/ExtraFile/search_2_file.dart
@@ -1,23 +1,67 @@
 import 'package:flutter/material.dart';
 
+// NOt WORKING
+
 class Search2File extends SearchDelegate {
+  List<String> data = [
+    "Tawhid",
+    "Abir",
+    "Lubnan",
+    "Khadiza",
+    "Khatun",
+    "Jakia",
+    "Mahrusha",
+    "Sawdan",
+    "Fahmid",
+    "Shohel",
+    "Miah"
+  ];
+
+  List<String> recentSearch = ["Tawhid", "Abir", "Shohel", "Miah"];
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return <Widget>[IconButton(onPressed: () {}, icon: Icon(Icons.clear))];
+    return <Widget>[
+      IconButton(
+          onPressed: () {
+            query = "";
+          },
+          icon: Icon(Icons.clear))
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back));
+    return IconButton(
+        onPressed: () => Navigator.pop(context), icon: Icon(Icons.arrow_back));
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return Text('data');
+    // ignore: unnecessary_null_comparison
+    if (query != null && data.contains(query.toLowerCase())) {
+      return ListTile(
+        title: Text(query),
+        onTap: () {},
+      );
+    } else if (query == "") {
+      return Text("");
+    } else {
+      return ListTile(
+        title: Text("No Result Found"),
+        onTap: () {},
+      );
+    }
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Text("");
+    return ListView.builder(
+        itemCount: recentSearch.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(recentSearch[index]),
+            trailing: Icon(Icons.arrow_forward_ios),
+          );
+        });
   }
 }
